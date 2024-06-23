e0f99eb2b616a5ab35c53825c572c3fe6bb36430
Huseyin Burak KURT <hsynbrkkrt@gmail.com>
2024-06-23 23:24:52 +0300

    pose is estimated.

--- Library/Extractor.py
+++ Library/Extractor.py
@@ -1,10 +1,47 @@
 import cv2
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
+import numpy as np
+
 
 class Extractor():
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create()
         self.last = None
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+    def extractRt(self, E):
+        W = np.mat([[0,-1,0],[1,0,0], [0,0,1]], dtype=float)
+        U,d,Vt = np.linalg.svd(E)
+        assert np.linalg.det(U) > 0
+        if np.linalg.det(Vt) < 0:
+            Vt *= -1.0
+        R = np.dot(np.dot(U,W), Vt)
+        if np.sum(R.diagonal()) < 0:
+            R = np.dot(np.dot(U,W.T), Vt)
+
+        t = U[:,2]
+
+        Rt = np.concatenate([R,t.reshape(3,1)], axis=1)
+
+        return Rt
+
+
+    # Turn [[x,y]] > [[x,y,1]]
+    def add_ones(self, x):
+        return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
+    def normalize(self, pt):
+        return np.dot(self.Kinv, self.add_ones(pt).T).T[:, 0:2]
+    
+    def denormalize(self, pt):
+        # return int(round(pt[0] + self.H/2)), int(round(pt[1] + self.W/2))
+        ret = np.dot(self.K, [pt[0], pt[1], 1.0])
+        # print(ret)
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, img):
         grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -24,9 +61,32 @@ class Extractor():
         if self.last is not None:
             matches = self.bf.knnMatch(des, self.last['des'], k=2)
             for m,n in matches:
-                if m.distance < 0.75*n.distance:
-                    ret.append((kps[m.queryIdx], self.last['kps'][m.trainIdx]))
-    
+                if m.distance < 0.6*n.distance:
+                    kp1 = kps[m.queryIdx].pt
+                    kp2 = self.last['kps'][m.trainIdx].pt
+                    ret.append((kp1, kp2))
+
+        # Filter
+        Rt = None
+        if len(ret) > 0:
+            ret = np.array(ret)
+            # print(ret.shape)
+             
+
+            # Normalize coords
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
+            model,  inliers = ransac((ret[:,0],ret[:,1]), 
+                                      EssentialMatrixTransform,
+                                    #   FundamentalMatrixTransform,
+                                      min_samples=8, 
+                                      residual_threshold=1, 
+                                      max_trials=500)    
+            # print(model.params)
+            ret = ret[inliers]
+            Rt = self.extractRt(model.params)
+
+        # Return
         self.last = {'kps': kps, 'des': des}
-        
-        return ret
+        return ret, Rt
--- Slam.py
+++ Slam.py
@@ -1,23 +1,32 @@
 import cv2
 from Library import Extractor
 from Utils import utils
+import numpy as np
 
 W = 256*2
 H = 144*2
 
-extractor = Extractor.Extractor()
+F = 160
+K = np.array([[F,0,W//2],[0,F,H//2], [0,0,1]])
+
+extractor = Extractor.Extractor(K)
 
 def image_process(img):
     img = cv2.resize(img, (W, H))
-    
-    mathes = extractor.extract(img)
 
-    print("%d mathes" %(len(mathes)))
+    matches, pose = extractor.extract(img)
 
-    for pt1, pt2 in mathes:
-        u1, v1 = map(lambda x: int(round(x)), pt1.pt)
-        u2, v2 = map(lambda x: int(round(x)), pt2.pt)
-        cv2.circle(img, (u1,v1), color=(0,255,0), radius=1)
-        cv2.line(img, (u1,v1), (u2,v2), color=(255,0,0))
+    if pose is None:
+        return
+
+    print("%d mathes" %(len(matches)))
+    print(pose)
+
+    for pt1, pt2 in matches:
+        u1, v1 = extractor.denormalize(pt1)
+        u2, v2 = extractor.denormalize(pt2)
+        cv2.circle(img, (u2,v2), color=(0,255,0), radius=1)
+        cv2.line(img, (u2,v2), (u1,v1), color=(255,0,0))
 
     utils.display(img)
+
